14ac9fb21a2a5abeafb1e96156f60c4ffeae286a
Raul <araulramos@gmail.com>
2021-03-06 16:33:45 -0600

    setup vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <test msg="testaroo"/>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -27,14 +28,21 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-  </div>
+    <test msg="testaroo"/>
+  </div>  
 </template>
 
 <script>
+
+import test from "./test/test.vue"
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    test
   }
 }
 </script>
